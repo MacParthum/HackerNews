7c564f8d48a7a6994cb3a86ed3dcb3e23e79bf5f
Mac Parthum <macparthum@gmail.com>
2020-10-04 23:18:45 -0400

    Updates

---
+++ precache-manifest.da9f488a3dc1d528e4fb86de47640668.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f27cedfd46a2cd9e505bdb644eada91e",
+    "url": "/HackerNews/index.html"
+  },
+  {
+    "revision": "2940c93284272969e893",
+    "url": "/HackerNews/static/js/2.406a9cca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HackerNews/static/js/2.406a9cca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6854da15c5a09eb6cbf",
+    "url": "/HackerNews/static/js/main.8c24121e.chunk.js"
+  },
+  {
+    "revision": "4aa3ebf27d5752937890",
+    "url": "/HackerNews/static/js/runtime-main.f2f1bf59.js"
+  }
+]);
