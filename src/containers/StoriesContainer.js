2750658f6e68b2e85013370cd8d1526b92b0e698
Mac Parthum <macparthum@gmail.com>
2020-10-04 23:53:06 -0400

    added hacker news header in storiesContainer.js

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { getStoryIds } from "../services/hnApi";
-import { Story } from '../components/Story';
+import { Story } from "../components/Story";
 
 export const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]);
 
   useEffect(() => {
     getStoryIds().then((data) => setStoryIds(data));
-}, []);
+  }, []);
 
-  return storyIds.map(storyId => <Story key={storyId} storyId={storyId} />);
-  
+  return (
+    <>
+      <h1>Hacker News Stories</h1>
+
+      {storyIds.map((storyId) => (
+        <Story key={storyId} storyId={storyId} />
+      ))}
+    </>
+  );
 };
